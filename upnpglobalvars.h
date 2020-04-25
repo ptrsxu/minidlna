69b0bb41a86f60f8b5434be656ab594a46b4bbef
Peter Xu <p@cooliktas.com>
2020-04-26 00:36:46 +0800

    add rmvb/rm support for minidlna version 1.2.1

--- upnpglobalvars.h
+++ upnpglobalvars.h
@@ -162,6 +162,7 @@
 	"http-get:*:video/x-flv:*," \
 	"http-get:*:video/x-tivo-mpeg:*," \
 	"http-get:*:video/quicktime:*," \
+	"http-get:*:video/x-pn-realvideo:*," \
 	"http-get:*:audio/mp4:*," \
 	"http-get:*:audio/x-wav:*," \
 	"http-get:*:audio/x-flac:*," \
--- utils.c
+++ utils.c
@@ -399,6 +399,7 @@ is_video(const char * file)
 		ends_with(file, ".m2t") || ends_with(file, ".mkv")   ||
 		ends_with(file, ".vob") || ends_with(file, ".ts")    ||
 		ends_with(file, ".flv") || ends_with(file, ".xvid")  ||
+		ends_with(file, ".rm") || ends_with(file, ".rmvb")  ||
 #ifdef TIVO_SUPPORT
 		ends_with(file, ".TiVo") ||
 #endif
